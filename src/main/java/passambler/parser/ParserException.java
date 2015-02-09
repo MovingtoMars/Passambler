b378b1c326d45b31cdde7f537f7c531dbe7278c7
Raoul <raoulvdberge@gmail.com>
2015-02-09 21:53:02 +0100

    Remove unused error

--- src/main/java/passambler/parser/ParserException.java
+++ src/main/java/passambler/parser/ParserException.java
@@ -15,8 +15,7 @@ public class ParserException extends Exception {
         UNEXPECTED_EOF("unexpected end of file"),
         UNEXPECTED_TOKEN("unexpected token %s"),
         UNSUPPORTED_OPERATOR("unsupported operator %s"),
-        ZERO_DIVISION("cannot divide by 0"),
-        NO_STR_REPRESENTATION("value has no string representation");
+        ZERO_DIVISION("cannot divide by 0");
 
         String message;
 
