03b4cf56e914f97f6983516dc5a2274e8a94fd8b
Raoul <raoulvdberge@gmail.com>
2015-02-11 20:46:48 +0100

    Add a bunch of math functions

--- src/main/java/passambler/function/Function.java
+++ src/main/java/passambler/function/Function.java
@@ -5,12 +5,58 @@ import passambler.parser.ParserException;
 import passambler.value.Value;
 
 public interface Function {
-    public static FunctionExit FUNCTION_EXIT = new FunctionExit();
-    public static FunctionRandom FUNCTION_RANDOM = new FunctionRandom();
-    public static FunctionSqrt FUNCTION_SQRT = new FunctionSqrt();
     public static FunctionWrite FUNCTION_WRITE = new FunctionWrite(false);
     public static FunctionWrite FUNCTION_WRITELN = new FunctionWrite(true);
     
+    public static FunctionMinMax FUNCTION_MIN = new FunctionMinMax(true);
+    public static FunctionMinMax FUNCTION_MAX = new FunctionMinMax(false);
+        
+    public static FunctionExit FUNCTION_EXIT = new FunctionExit();
+    public static FunctionRandom FUNCTION_RANDOM = new FunctionRandom();
+    public static FunctionSqrt FUNCTION_SQRT = new FunctionSqrt();
+    
+    public static FunctionBasicMath FUNCTION_ABS = new FunctionBasicMath() {
+        @Override
+        public double getValue(double input) {
+            return Math.abs(input);
+        }
+    };
+    
+    public static FunctionBasicMath FUNCTION_SIN = new FunctionBasicMath() {
+        @Override
+        public double getValue(double input) {
+            return Math.sin(input);
+        }
+    };
+    
+    public static FunctionBasicMath FUNCTION_COS = new FunctionBasicMath() {
+        @Override
+        public double getValue(double input) {
+            return Math.cos(input);
+        }
+    };
+    
+    public static FunctionBasicMath FUNCTION_TAN = new FunctionBasicMath() {
+        @Override
+        public double getValue(double input) {
+            return Math.tan(input);
+        }
+    };
+    
+    public static FunctionBasicMath FUNCTION_FLOOR = new FunctionBasicMath() {
+        @Override
+        public double getValue(double input) {
+            return Math.floor(input);
+        }
+    };
+    
+    public static FunctionBasicMath FUNCTION_CEIL = new FunctionBasicMath() {
+        @Override
+        public double getValue(double input) {
+            return Math.ceil(input);
+        }
+    };
+
     public int getArguments();
 
     public boolean isArgumentValid(Value value, int argument);
--- src/main/java/passambler/parser/Scope.java
+++ src/main/java/passambler/parser/Scope.java
@@ -2,15 +2,9 @@ package passambler.parser;
 
 import java.util.HashMap;
 import java.util.Map;
-import passambler.function.FunctionExit;
-import passambler.function.FunctionSqrt;
 import passambler.function.Function;
-import passambler.function.FunctionRandom;
-import passambler.function.FunctionWrite;
 import passambler.value.Value;
 import passambler.value.ValueBlock;
-import passambler.value.ValueBool;
-import passambler.value.ValueNum;
 
 public class Scope {
     private Scope parent;
@@ -31,6 +25,15 @@ public class Scope {
         setSymbol("random", Function.FUNCTION_RANDOM);
         setSymbol("write", Function.FUNCTION_WRITE);
         setSymbol("writeln", Function.FUNCTION_WRITELN);
+        setSymbol("abs", Function.FUNCTION_ABS);
+        setSymbol("sin", Function.FUNCTION_SIN);
+        setSymbol("cos", Function.FUNCTION_COS);
+        setSymbol("tan", Function.FUNCTION_TAN);
+        setSymbol("floor", Function.FUNCTION_FLOOR);
+        setSymbol("ceil", Function.FUNCTION_CEIL);
+        setSymbol("min", Function.FUNCTION_MIN);
+        setSymbol("max", Function.FUNCTION_MAX);
+        
         setSymbol("nil", Value.VALUE_NIL);
         setSymbol("pi", Value.VALUE_PI);
         setSymbol("true", Value.VALUE_TRUE);
