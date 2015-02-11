12b504dec5dfb04ffc5f54f143a0e3ef35a98b2d
Raoul <raoulvdberge@gmail.com>
2015-02-11 18:38:56 +0100

    Refactor main file and exception handling. This makes it so that in interactive mode, the program won't quit when an exception occurs.

--- src/main/java/passambler/Main.java
+++ src/main/java/passambler/Main.java
@@ -19,78 +19,90 @@ import passambler.value.Value;
 
 public class Main {
     public static final String VERSION = "0.1.0-SNAPSHOT";
-    
+
     public static final Logger LOGGER = Logger.getLogger("Passambler");
 
     public static void main(String[] args) {
-        OptionParser optionParser = new OptionParser();
-        
-        optionParser.accepts("v", "Version number");
-        optionParser.accepts("h", "Help");
-        optionParser.accepts("a", "Run interactively");
-        optionParser.accepts("s", "Shows the stacktrace of the parser");
-        optionParser.accepts("f", "Parse and execute a file").withRequiredArg();
-        optionParser.accepts("t", "Show tokens");
-        
         try {
-            OptionSet options = optionParser.parse(args);
-            
+            OptionParser optionParser = new OptionParser();
+
+            optionParser.accepts("v", "Version number");
+            optionParser.accepts("h", "Help");
+            optionParser.accepts("a", "Run interactively");
+            optionParser.accepts("s", "Shows the stacktrace of the parser");
+            optionParser.accepts("f", "Parse and execute a file").withRequiredArg();
+            optionParser.accepts("t", "Show tokens");
+
+            OptionSet options = null;
+
+            options = optionParser.parse(args);
+
             LOGGER.setUseParentHandlers(false);
             LOGGER.addHandler(new LogHandler(options.has("s")));
-            
+
             if (options.has("v")) {
                 LOGGER.log(Level.INFO, String.format("Passambler %s", VERSION));
             }
-            
+
             if (options.has("h") || !options.hasOptions()) {
                 optionParser.printHelpOn(System.out);
             }
-                       
+
             if (options.has("f")) {
-                Lexer lexer = new Lexer(String.join("\n", Files.readAllLines(Paths.get(String.valueOf(options.valueOf("f"))), Charset.forName("UTF-8"))));
-                
-                if (options.has("t")) {
-                    for (Token token : lexer.scan()) {
-                        LOGGER.log(Level.INFO, token.toString());
+                try {
+                    Lexer lexer = new Lexer(String.join("\n", Files.readAllLines(Paths.get(String.valueOf(options.valueOf("f"))), Charset.forName("UTF-8"))));
+
+                    if (options.has("t")) {
+                        for (Token token : lexer.scan()) {
+                            LOGGER.log(Level.INFO, token.toString());
+                        }
+                    } else {
+                        Parser parser = new Parser();
+
+                        parser.getScope().addStd();
+
+                        parser.parseLexer(lexer);
                     }
-                } else {
-                    Parser parser = new Parser();
-
-                    parser.getScope().addStd();
-
-                    parser.parseLexer(lexer);
+                } catch (LexerException e) {
+                    LOGGER.log(Level.SEVERE, "Lexer exception", e);
+                } catch (ParserException e) {
+                    LOGGER.log(Level.SEVERE, "Parser exception", e);
+                } catch (RuntimeException e) {
+                    LOGGER.log(Level.SEVERE, "Runtime exception", e);
                 }
             }
-            
+
             if (options.has("a")) {
                 Parser parser = new Parser();
-                
+
                 parser.getScope().addStd();
 
                 Scanner input = new Scanner(System.in);
 
                 while (true) {
                     System.out.print("~> ");
-                    
-                    String line = input.nextLine();
 
-                    Value result = parser.parseLexer(new Lexer(line));
-                    
+                    Value result = null;
+
+                    try {
+                        result = parser.parseLexer(new Lexer(input.nextLine()));
+                    } catch (LexerException e) {
+                        LOGGER.log(Level.WARNING, "Lexer exception", e);
+                    } catch (ParserException e) {
+                        LOGGER.log(Level.WARNING, "Parser exception", e);
+                    } catch (RuntimeException e) {
+                        LOGGER.log(Level.WARNING, "Runtime exception", e);
+                    }
+
                     if (result != null) {
                         System.out.println(result);
                     }
                 }
             }
-        } catch (LexerException e) {
-            LOGGER.log(Level.SEVERE, "Lexer exception", e);
-        } catch (ParserException e) {
-            LOGGER.log(Level.SEVERE, "Parser exception", e);
-        } catch (OptionException e) {
-            LOGGER.log(Level.SEVERE, "Option exception", e);
         } catch (IOException e) {
             LOGGER.log(Level.SEVERE, "I/O exception", e);
-        } catch (RuntimeException e) {
-            LOGGER.log(Level.SEVERE, "Runtime exception", e);
+        } catch (OptionException e) {
+            LOGGER.log(Level.SEVERE, "Option exception", e);
         }
     }
 }
