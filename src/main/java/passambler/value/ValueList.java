3d025b4d950ac60c600b1ef8196aa4096ec225f4
Raoul <raoulvdberge@gmail.com>
2015-02-20 22:15:40 +0100

    Unused imports

--- src/main/java/passambler/value/ValueList.java
+++ src/main/java/passambler/value/ValueList.java
@@ -2,9 +2,6 @@ package passambler.value;
 
 import java.util.ArrayList;
 import java.util.List;
-import passambler.procedure.Procedure;
-import passambler.parser.Parser;
-import passambler.parser.ParserException;
 
 public class ValueList extends Value implements IndexedValue {
     protected List<Value> list = new ArrayList<>();
