5dedd1a21334f580868c9d81c488f75b470f424f
Raoul <raoulvdberge@gmail.com>
2015-02-24 19:51:32 +0100

    Update write Issue #25 - Arguments in write / writeln should be seperated by a space

--- src/main/java/passambler/pkg/std/function/FunctionWrite.java
+++ src/main/java/passambler/pkg/std/function/FunctionWrite.java
@@ -33,6 +33,10 @@ public class FunctionWrite extends Function {
             }
 
             out.getStream().print(argument);
+            
+            if (argument != arguments[arguments.length - 1]) {
+                out.getStream().print(" ");
+            }
         }
 
         if (newLine) {
