b5e643fa40791906338120a31775a7e775237b1c
Raoul <raoulvdberge@gmail.com>
2015-02-27 01:39:59 +0100

    Add reading capabilities to a socket

--- src/main/java/passambler/pkg/net/value/ValueSocket.java
+++ src/main/java/passambler/pkg/net/value/ValueSocket.java
@@ -1,12 +1,15 @@
 package passambler.pkg.net.value;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.Socket;
+import passambler.value.ReadHandler;
 import passambler.value.Value;
 import passambler.value.ValueStr;
 import passambler.value.WriteHandler;
 
-public class ValueSocket extends Value implements WriteHandler {
+public class ValueSocket extends Value implements WriteHandler, ReadHandler {
     private Socket socket;
 
     public ValueSocket(Socket socket) {
@@ -26,6 +29,15 @@ public class ValueSocket extends Value implements WriteHandler {
         try {
             socket.getOutputStream().write(value.getValue().toString().getBytes());
         } catch (IOException e) {
+        }
+    }
+
+    @Override
+    public Value read() {
+        try {
+            return new ValueStr(new BufferedReader(new InputStreamReader(socket.getInputStream())).readLine());
+        } catch (IOException e) {
+            return Value.VALUE_NIL;
         }
     }
 }
