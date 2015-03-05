a613f008ff3732ffc7dd494af3db6926d1f7e8a1
Raoul <raoulvdberge@gmail.com>
2015-03-05 18:09:41 +0100

    Reformat token

--- src/main/java/passambler/lexer/Token.java
+++ src/main/java/passambler/lexer/Token.java
@@ -3,26 +3,27 @@ package passambler.lexer;
 public class Token {
     public enum Type {
         IDENTIFIER, STRING, NUMBER,
-        LPAREN, RPAREN,
-        LBRACKET, RBRACKET,
-        COMMA,
-        WHILE, FOR,
-        PERIOD, RANGE,
+        LPAREN, RPAREN, LBRACKET, RBRACKET, LBRACE, RBRACE,
         ASSIGN, ASSIGN_PLUS, ASSIGN_MINUS, ASSIGN_MULTIPLY, ASSIGN_DIVIDE, ASSIGN_POWER, ASSIGN_MODULO,
         PLUS, MINUS, MULTIPLY, DIVIDE, POWER, MODULO, COMPARE,
-        SEMI_COL, COL,
-        LBRACE, RBRACE,
         EQUAL, NEQUAL, GT, LT, GTE, LTE, NOT,
-        AND, OR, TERNARY,
-        FN, RETURN,
-        IF, ELSEIF, ELSE;
+        AND, OR,
+        TERNARY,
+        WHILE, FOR, FN, RETURN, IF, ELSEIF, ELSE,
+        RANGE,
+        PERIOD, COMMA, SEMI_COL, COL;
 
         public boolean isOperator() {
-            return this == PLUS || this == MINUS || this == MULTIPLY || this == DIVIDE || this == POWER || this == MODULO || this == GT || this == LT || this == GTE || this == LTE || this == EQUAL || this == NEQUAL || this == AND || this == OR || this == COMPARE || this == RANGE;
+            return this == PLUS || this == MINUS || this == MULTIPLY || this == DIVIDE || this == POWER || this == MODULO
+                || this == GT || this == LT || this == GTE || this == LTE
+                || this == EQUAL || this == NEQUAL
+                || this == AND || this == OR
+                || this == COMPARE || this == RANGE;
         }
 
         public boolean isAssignmentOperator() {
-            return this == ASSIGN || this == ASSIGN_PLUS || this == ASSIGN_MINUS || this == ASSIGN_MULTIPLY || this == ASSIGN_DIVIDE || this == ASSIGN_POWER || this == ASSIGN_MODULO;
+            return this == ASSIGN
+                || this == ASSIGN_PLUS || this == ASSIGN_MINUS || this == ASSIGN_MULTIPLY || this == ASSIGN_DIVIDE || this == ASSIGN_POWER || this == ASSIGN_MODULO;
         }
     }
 
