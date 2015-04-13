a84d2032f2f90953ce868254a6014277ca2938d4
Raoul <raoulvdberge@gmail.com>
2015-04-13 23:24:17 +0200

    Rename net/http package classes

---
+++ src/main/java/passambler/pack/net/http/function/FileHandlerFunction.java
@@ -0,0 +1,25 @@
+package passambler.pack.net.http.function;
+
+import passambler.value.function.Function;
+import passambler.value.function.FunctionContext;
+import passambler.pack.net.http.value.FileHandlerValue;
+import passambler.exception.EngineException;
+import passambler.value.Value;
+import passambler.value.StringValue;
+
+public class FileHandlerFunction extends Value implements Function {
+    @Override
+    public int getArguments() {
+        return 1;
+    }
+
+    @Override
+    public boolean isArgumentValid(Value value, int argument) {
+        return value instanceof StringValue;
+    }
+
+    @Override
+    public Value invoke(FunctionContext context) throws EngineException {
+        return new FileHandlerValue(((StringValue) context.getArgument(0)).getValue());
+    }
+}
