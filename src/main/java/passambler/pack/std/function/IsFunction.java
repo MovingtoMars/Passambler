6ceab12be6b1174d3448fb5168c9a1a8130972c6
Raoul <raoulvdberge@gmail.com>
2015-04-16 17:35:01 +0200

    std: Add "Is" functions, to check if a value matches a type

---
+++ src/main/java/passambler/pack/std/function/IsFunction.java
@@ -0,0 +1,30 @@
+package passambler.pack.std.function;
+
+import passambler.exception.EngineException;
+import passambler.value.BooleanValue;
+import passambler.value.Value;
+import passambler.value.function.Function;
+import passambler.value.function.FunctionContext;
+
+public class IsFunction extends Value implements Function {
+    private Class<? extends Value> classMatch;
+
+    public IsFunction(Class<? extends Value> classMatch) {
+        this.classMatch = classMatch;
+    }
+
+    @Override
+    public int getArguments() {
+        return 1;
+    }
+
+    @Override
+    public boolean isArgumentValid(Value value, int argument) {
+        return true;
+    }
+
+    @Override
+    public Value invoke(FunctionContext context) throws EngineException {
+        return new BooleanValue(context.getArgument(0).getClass().equals(classMatch));
+    }
+}
