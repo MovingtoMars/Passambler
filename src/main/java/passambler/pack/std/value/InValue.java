b291871e0dedc19c78f7188e0ca82273c103cd4e
Raoul <raoulvdberge@gmail.com>
2015-04-16 19:19:39 +0200

    std: Use System.console() on In value

--- src/main/java/passambler/pack/std/value/InValue.java
+++ src/main/java/passambler/pack/std/value/InValue.java
@@ -1,6 +1,5 @@
 package passambler.pack.std.value;
 
-import java.util.Scanner;
 import passambler.exception.EngineException;
 import passambler.value.ReadableValue;
 import passambler.value.StringValue;
@@ -9,6 +8,6 @@ import passambler.value.Value;
 public class InValue extends Value implements ReadableValue {
     @Override
     public Value read() throws EngineException {
-        return new StringValue(new Scanner(System.in).nextLine());
+        return new StringValue(System.console().readLine());
     }
 }
