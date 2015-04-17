bfd56a7022649abebde68d9d1d3422d00c299341
Raoul <raoulvdberge@gmail.com>
2015-04-17 14:12:52 +0200

    This *should* fix operator precedence

--- src/main/java/passambler/parser/expression/ExpressionParser.java
+++ src/main/java/passambler/parser/expression/ExpressionParser.java
@@ -115,11 +115,10 @@ public class ExpressionParser {
 
         Value value = null;
 
-        performPrecedence(values, TokenType.RANGE);
-        performPrecedence(values, TokenType.COMPARE);
         performPrecedence(values, TokenType.POWER, TokenType.MODULO);
         performPrecedence(values, TokenType.MULTIPLY, TokenType.DIVIDE);
         performPrecedence(values, TokenType.PLUS, TokenType.MINUS);
+        performPrecedence(values, TokenType.RANGE, TokenType.COMPARE);
         performPrecedence(values, TokenType.GT, TokenType.GTE, TokenType.LT, TokenType.LTE);
         performPrecedence(values, TokenType.EQUAL, TokenType.NEQUAL);
         performPrecedence(values, TokenType.AND, TokenType.OR);
