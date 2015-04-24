b2914c0b7317427785b343f2d217081491bb630c
Raoul <raoulvdberge@gmail.com>
2015-04-24 16:35:31 +0200

    Eval errors should be catchable Issue #29 - std: Eval should throw error exception on engine exception

--- src/main/java/passambler/bundle/std/function/EvalFunction.java
+++ src/main/java/passambler/bundle/std/function/EvalFunction.java
@@ -4,6 +4,7 @@ import passambler.value.function.Function;
 import passambler.value.function.FunctionContext;
 import passambler.lexer.Lexer;
 import passambler.exception.EngineException;
+import passambler.exception.ErrorException;
 import passambler.value.Value;
 import passambler.value.StringValue;
 
@@ -20,6 +21,10 @@ public class EvalFunction extends Value implements Function {
 
     @Override
     public Value invoke(FunctionContext context) throws EngineException {
-        return context.getParser().parse(new Lexer(((StringValue) context.getArgument(0)).toString()).scan());
+        try {
+            return context.getParser().parse(new Lexer(((StringValue) context.getArgument(0)).toString()).scan());
+        } catch (EngineException e) {
+            throw new ErrorException(e);
+        }
     }
 }
