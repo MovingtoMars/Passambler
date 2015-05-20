073b2b1b1ca21dfa08152d8c85dc9fea38aa2d79
Raoul Van den Berge <raoulvdberge@gmail.com>
2015-05-20 20:17:12 +0200

    std: Improve contains Task #32 - std: Add contains method to check for arrays / dict items

--- src/main/java/passambler/bundle/std/function/ContainsFunction.java
+++ src/main/java/passambler/bundle/std/function/ContainsFunction.java
@@ -1,10 +1,13 @@
 package passambler.bundle.std.function;
 
 import passambler.exception.EngineException;
+import passambler.util.ValueConstants;
 import passambler.value.function.Function;
 import passambler.value.function.FunctionContext;
 import passambler.value.Value;
 import passambler.value.BooleanValue;
+import passambler.value.DictValue;
+import passambler.value.ListValue;
 import passambler.value.StringValue;
 
 public class ContainsFunction extends Value implements Function {
@@ -15,11 +18,30 @@ public class ContainsFunction extends Value implements Function {
 
     @Override
     public boolean isArgumentValid(Value value, int argument) {
-        return value instanceof StringValue;
+        if (argument == 0) {
+            return value instanceof ListValue || value instanceof DictValue || value instanceof StringValue;
+        }
+        
+        return true;
     }
 
     @Override
     public Value invoke(FunctionContext context) throws EngineException {
-        return new BooleanValue(((StringValue) context.getArgument(0)).toString().contains(((StringValue) context.getArgument(1)).toString()));
+        Value value = context.getArgument(0);
+        
+        if (value instanceof StringValue) {
+            return new BooleanValue(((StringValue) value).toString().contains(context.getArgument(1).toString()));
+        } else if (value instanceof ListValue) {
+            return new BooleanValue(((ListValue) value).getValue().contains(context.getArgument(1)));
+        } else if (value instanceof DictValue) {
+            for (Value entry : ((DictValue) value).getValue().values()) {
+                if (entry.equals(context.getArgument(1))) {
+                    return ValueConstants.TRUE;
+                }
+            }
+            return ValueConstants.FALSE;
+        }
+        
+        return null;
     }
 }
