03e628e41445f2cadd99e3c0b51239b850c5e970
Raoul Van den Berge <raoulvdberge@gmail.com>
2015-05-27 22:48:09 +0200

    Clarify

--- src/main/java/passambler/parser/expression/feature/IfFeature.java
+++ src/main/java/passambler/parser/expression/feature/IfFeature.java
@@ -13,6 +13,11 @@ public class IfFeature implements Feature {
 
     @Override
     public Value perform(ExpressionParser parser, Value currentValue) throws EngineException {
+        /*
+         * The if expression actually shares the same code with the if statement
+         * because due to the fact that expressions can't return anything to the scope,
+         * we have to keep having actual if statements.
+         */
         return new passambler.parser.feature.IfFeature().perform(parser.getParser(), parser.getTokens());
     }
 }
