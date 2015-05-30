7a60dbdcd58df38bb67ce5ebc5ffe2553fb5c2e2
Raoul Van den Berge <raoulvdberge@gmail.com>
2015-05-30 20:49:15 +0200

    Print to err

--- src/main/java/passambler/Main.java
+++ src/main/java/passambler/Main.java
@@ -64,7 +64,7 @@ public class Main {
         }
 
         if (options.has("h") || (!options.hasOptions() && options.nonOptionArguments().isEmpty())) {
-            optionParser.printHelpOn(System.out);
+            optionParser.printHelpOn(System.err);
         }
     }
 
